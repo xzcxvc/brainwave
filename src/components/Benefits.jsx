deb19792840a7f74732aee1c81c051493bf45721
xzcxvc <daryl.tiamps@gmail.com>
2025-02-05 11:05:45 +0800

    add footer

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -8,7 +8,7 @@ import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
-    <Section className="features">
+    <Section id="features">
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl text-center"
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -21,8 +21,8 @@ const Section = ({
     >
       {children}
 
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none mb:block lg:left-7.5 xl:left-10"></div>
-      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none mb:block lg:right-7.5 xl:right-10"></div>
+      <div className="hidden lg:block absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10"></div>
+      <div className="hidden lg:block absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10"></div>
 
       {crosses && (
         <>
